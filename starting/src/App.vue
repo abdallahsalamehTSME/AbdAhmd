286250bca57f98829efbdbe5c3761ab2fd03e9b1
abdallahSalameh <abdallah.salameh@cubesplatform.com>
2023-08-20 13:09:04 +0300

    Edit App

--- starting/src/App.vue
+++ starting/src/App.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import Container from './components/Container.vue'
+import { RouterLink, RouterView } from "vue-router";
+import Container from "./components/Container.vue";
+import FormsContainer from "./components/FormsContainer.vue";
+import Navigation from "./components/Navigation.vue";
 </script>
 
 <template>
   <div class="container">
-    <Container/>
-
+    <Container>
+      <Navigation></Navigation>
+      <FormsContainer><div>forms will be here</div></FormsContainer>
+    </Container>
   </div>
 </template>
 
 <style scoped>
-.container{
+.container {
 }
 </style>
