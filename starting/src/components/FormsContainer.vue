90777c8b36dfc82b1dd54eaea279247ea3dfc384
abdallahSalameh <abdallah.salameh@cubesplatform.com>
2023-08-20 13:09:15 +0300

    Add FormsContainer

---
+++ starting/src/components/FormsContainer.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  border: 1px solid peru;
+  width: 70%;
+  height: auto;
+}
+</style>
